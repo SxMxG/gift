1d8f8a00f44fb54993b7a111815bb1f543db7951
SxMxG <sxmxg01@gmail.com>
2022-10-20 04:24:38 +0000

    Main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<!--<h1>Hello Anthony and Emily</h1>
+<h1>Hello Anthony and Emily</h1>
 <p>This is a test website thing to show during diner it might freak you out tho</p>
--->
-<h1>Thank you Noah for helping me</h1>
+<!--<h1>Thank you Noah for helping me</h1>-->
 <style>
     h1{
         color:purple;
