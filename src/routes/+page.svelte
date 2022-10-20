42ec3f777be4c97fcdc337dbf2523819b9221851
SxMxG <sxmxg01@gmail.com>
2022-10-20 04:22:53 +0000

    Main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--<h1>Hello Anthony and Emily</h1>
+<p>This is a test website thing to show during diner it might freak you out tho</p>
+-->
+<h1>Thank you Noah for helping me</h1>
+<style>
+    h1{
+        color:purple;
+        text-align: center;
+    }
+    p{
+        color:rosybrown;
+        text-align: center;
+    }
+</style>
